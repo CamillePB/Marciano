545ed64978088649dac8c6427969b924c13fd2fe
CamillePB <camille.pedro.bueno@hotmail.com>
2023-03-23 23:48:23 -0300

    comitado

---
+++ src/marciano/Marciano.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marciano;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Marciano {
+  int cont=0;
+    
+    public static void main(String[] args) {
+        marciano2 x = new marciano2();
+        x.setVisible(true);
+        
+       
+        
+    }
+    
+}
